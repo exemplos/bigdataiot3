217f1e286fc0a01cbd50f1371b767635c7ec39f1
Francisco Calaca <chicocx@gmail.com>
2018-05-12 13:45:42 -0300

    codigo inicial

--- mqtt_bigdata_3_iot.ino
+++ mqtt_bigdata_3_iot.ino
@@ -2,10 +2,24 @@
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 
+const char* ssid     = "iot";
+const char* password = "12348765";
+
+WiFiClient CLIENT;
+PubSubClient MQTT(CLIENT);
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
